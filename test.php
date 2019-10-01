96a4323e463aa5fe7dba2bf334230a8d2970262c
jariah jaramillo <jariahjaramillo@yahoo.com>
2019-09-30 21:13:56 -0600

    improvements to website finding player stats

--- test.php
+++ test.php
@@ -1,9 +1,16 @@
 <?php 
-$player = $_POST['player'];
-
  
 $player = $_POST['player'];
 
 $result = file_get_contents("https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=".$player);
-var_dump($result);
+//var_dump($result);
+for ($i = 0; $i < 494; $i++){
+    if ($result[$i] === ',' or $result[$i] === ' '){
+        echo "<br>";
+    }
+    else {
+        echo $result[$i];
+    }
+}
+
 ?>
